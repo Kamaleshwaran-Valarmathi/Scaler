05b63e516b740985c06de2ee2ced96b4f382c359
Kamaleshwaran <124665343+Kamaleshwaran-Valarmathi@users.noreply.github.com>
2024-10-29 19:13:25 +0530

    Create Solution.java

---
+++ Advanced DSA/01. Time Complexity/01. Count Factors - 2/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int solve(int A) {
+        int factors_count = 0;
+        for (int i = 1; i*i <= A; ++i)
+            if (A % i == 0)
+                factors_count += ((A / i == i) ? 1 : 2);
+        return factors_count;
+    }
+}
